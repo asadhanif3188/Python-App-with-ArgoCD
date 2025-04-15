23edd8a93078ec888bb26f21242dbe2c260b3c4d
Asad Hanif <asadhanif@biome.io>
2025-04-15 16:12:17 +0500

    APIs are corrected and Endpoints are mentioned

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Python-App-with-ArgoCD
+
+Endpoints will be available at:
+- GET `http://localhost:5000/health`
+- GET `http://localhost:5000/info`
+- GET `http://localhost:5000/time`
---
+++ src/app.py
@@ -0,0 +1,41 @@
+# app.py
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+import datetime
+import os
+import platform
+
+app = Flask(__name__)
+api = Api(app)
+
+class HealthCheck(Resource):
+    def get(self):
+        return {
+            'status': 'healthy',
+            'version': '1.0.1',
+            'environment': os.getenv('FLASK_ENV', 'development')
+        }
+
+class ServerInfo(Resource):
+    def get(self):
+        return {
+            'python_version': platform.python_version(),
+            'hostname': platform.node(),
+            'system': platform.system()
+        }
+
+class CurrentTime(Resource):
+    def get(self):
+        return {
+            'utc_time': datetime.datetime.now(datetime.timezone.utc).astimezone().isoformat(),
+            'local_time': datetime.datetime.now().astimezone().isoformat()
+        }
+
+api.add_resource(HealthCheck, '/health')
+api.add_resource(ServerInfo, '/info')
+api.add_resource(CurrentTime, '/time')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.getenv('FLASK_ENV') != 'production'
+    app.run(host='0.0.0.0', port=port, debug=debug)
